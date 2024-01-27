346c882d8e55f1c5876564c081ac837e9c82dcdc
Gustavo Antonio <gustavo1998antonio@gmail.com>
2024-01-27 10:47:16 -0300

    chore: use custom link

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -1,8 +1,7 @@
 import { Card } from "@/components/Card";
+import { CustomLink } from "@/components/CustomLink";
 import { MotionWrapper } from "@/components/MotionWrapper";
 import { CERTIFICATIONS, EDUCATION_EXPERIENCE } from "@/utils/constants";
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
 
 export default function Experience() {
   return (
@@ -33,18 +32,9 @@ export default function Experience() {
               subtitle={`${experience.organization} ~ ${experience.date}`}
             >
               <div className="text-sm">
-                <Link
-                  target="_blank"
-                  href={experience.credential}
-                  className="flex gap-2 items-center hover:underline underline-offset-4"
-                >
-                  Show credential{" "}
-                  <ArrowTopRightOnSquareIcon
-                    width={20}
-                    height={20}
-                    className="w-5 h-5"
-                  />
-                </Link>
+                <CustomLink target="_blank" href={experience.credential}>
+                  Show credential
+                </CustomLink>
               </div>
             </Card>
           ))}
