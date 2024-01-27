553dfbee7a1311f6521cc7de6a663b74d122e696
Gustavo Antonio <gustavo1998antonio@gmail.com>
2024-01-27 10:46:14 -0300

    chore: add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import { NavContent } from "@/components/NavContent";
 import { NavBottom } from "@/components/NavBottom";
+import { CustomLink } from "@/components/CustomLink";
+import Link from "next/link";
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { GithubIcon } from "@/icons/Github";
 
 export const metadata: Metadata = {
-  title: "Gustavo Lara González (Flan) - Software Engineer",
+  title: "Gustavo Lara González (Flan) ~ Software Engineer",
   description:
     "Experienced Software Engineer specializing in both frontend and backend development. Explore my portfolio to see innovative projects, technical skills, and my journey in tackling complex challenges in the tech world.",
 };
@@ -26,7 +29,49 @@ export default function RootLayout({
           <aside className="hidden sticky top-0 lg:flex lg:flex-col lg:gap-4 w-[25rem] bg-gray-900 border-r border-gray-800 p-4 min-h-screen h-full">
             <NavContent />
           </aside>
-          <div className="w-full p-8">{children}</div>
+          <div className="w-full p-8">
+            {children}
+            <footer className="w-full mt-12 -mb-4">
+              <div className="flex flex-wrap justify-between items-center">
+                <div className="text-sm">
+                  Created with{" "}
+                  <Link
+                    target="_blank"
+                    href="https://nextjs.org/blog/next-14"
+                    className="hover:underline underline-offset-4"
+                  >
+                    NextJS 14
+                  </Link>
+                  ,{" "}
+                  <Link
+                    target="_blank"
+                    href="https://nextjs.org/blog/next-14"
+                    className="hover:underline underline-offset-4"
+                  >
+                    TailwindCSS
+                  </Link>
+                  ,{" "}
+                  <Link
+                    target="_blank"
+                    href="https://nextjs.org/blog/next-14"
+                    className="hover:underline underline-offset-4"
+                  >
+                    Typescript
+                  </Link>{" "}
+                  and a lot of 🍮
+                </div>
+                <div className="text-sm">
+                  <CustomLink
+                    href="https://github.com/flaancs/portfolio"
+                    target="_blank"
+                  >
+                    <GithubIcon width={20} height={20} />
+                    Source code
+                  </CustomLink>
+                </div>
+              </div>
+            </footer>
+          </div>
         </div>
       </body>
     </html>
