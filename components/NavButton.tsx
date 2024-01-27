d9aa7d2978b862bc752df757550975a55a9ef3cc
Gustavo Antonio <gustavo1998antonio@gmail.com>
2024-01-27 12:08:51 -0300

    chore: fix selected nav

--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -19,9 +19,15 @@ export function NavButton({
   onNavigate,
 }: NavButtonProps) {
   const pathname = usePathname();
+  const segments = pathname.split("/");
+  const page = pathname.split("/")[1];
+
   const linkClassName = classNames(
     "hover:bg-gray-700 py-2 px-4 rounded flex gap-4 items-center",
-    { "bg-gray-700": pathname === path }
+    {
+      "bg-gray-700":
+        segments.length > 2 ? path.includes(page) : pathname === path,
+    }
   );
 
   return (
