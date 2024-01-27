36ae38d1830d3fafdeb433eb0e9f0e5b0eea0fe9
Gustavo Antonio <gustavo1998antonio@gmail.com>
2024-01-27 10:51:18 -0300

    chore: add important to bg color

--- components/StackCard.tsx
+++ components/StackCard.tsx
@@ -7,7 +7,7 @@ export interface StackCardProps {
 
 export function StackCard({ name, icon }: StackCardProps) {
   return (
-    <Card className="flex items-center justify-center bg-gray-800 md:w-24 md:h-24">
+    <Card className="flex items-center justify-center !bg-gray-800 md:w-24 md:h-24">
       <div className="flex flex-col items-center justify-center text-center gap-1">
         {icon}
         <p>{name}</p>
