205c40edc2fb07cd2bfc5c0402571991aefdca07
Gustavo Antonio <gustavo1998antonio@gmail.com>
2024-01-31 13:22:38 -0300

    chore: add post image and set it as og image

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -3,8 +3,9 @@ import { MotionWrapper } from "@/components/MotionWrapper";
 import { POSTS } from "@/utils/constants";
 import { Markdown } from "@/components/Markdown";
 import { Metadata } from "next";
-import { readFileSync } from 'fs';
-import path from 'path';
+import { readFileSync } from "fs";
+import Image from "next/image";
+import path from "path";
 
 type Props = {
   params: { slug: string };
@@ -19,22 +20,38 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     openGraph: {
       title: `flaancs.dev - ${postInfo?.title}`,
       description: postInfo?.description,
-      images: ["https://www.flaancs.dev/images/og.png"],
+      images: [`https://www.flaancs.dev${postInfo?.image}`],
     },
   };
 }
 
 export default async function Post({ params }: { params: { slug: string } }) {
-  const file = path.join(process.cwd(), 'posts', `${params.slug}.md`);
-  const data = readFileSync(file, 'utf8');
-  const postInfo = POSTS.find((post) => post.id === params.slug);
+  const file = path.join(process.cwd(), "posts", `${params.slug}.md`);
+  const data = readFileSync(file, "utf8");
+  const postInfo = POSTS.find((post) => post.id === params.slug)!;
 
   return (
     <MotionWrapper>
       <div className="flex flex-col gap-10">
         <div className="flex flex-col gap-4">
-          <Card title={postInfo?.title} subtitle={postInfo?.readTime}>
-            <Markdown source={data} />
+          <Card
+            title={postInfo.title}
+            subtitle={postInfo.readTime}
+            titleClassName="text-xl"
+          >
+            <div className="flex flex-col gap-4">
+              <div className="w-full flex justify-center">
+                <Image
+                  src={postInfo.image}
+                  alt={postInfo.title}
+                  height={630}
+                  width={1200}
+                  layout="responsive"
+                  className="rounded-lg max-h-[630px] max-w-[1200px] object-cover"
+                />
+              </div>
+              <Markdown source={data} />
+            </div>
           </Card>
         </div>
       </div>
