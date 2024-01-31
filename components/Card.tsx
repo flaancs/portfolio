bbfc93300cd7362de87408e486f8604b9d6fbc97
Gustavo Antonio <gustavo1998antonio@gmail.com>
2024-01-31 13:22:52 -0300

    chore: add titleClassName and onClick props

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,18 +4,37 @@ export interface CardProps {
   title?: string;
   subtitle?: string;
   className?: string;
+  titleClassName?: string;
   children?: React.ReactNode;
+  onClick?: () => void;
 }
 
-export function Card({ title, subtitle, className, children }: CardProps) {
+export function Card({
+  title,
+  subtitle,
+  className,
+  titleClassName,
+  children,
+  onClick,
+}: CardProps) {
   const cardClassName = classnames(
-    "bg-gray-900 hover:bg-gray-800 rounded p-4",
+    "text-left bg-gray-900 hover:bg-gray-800 rounded p-4",
     className
   );
-  
-  return (
-    <div className={cardClassName}>
-      <p className="font-semibold text-md">{title}</p>
+  const cardTitleClassName = classnames(
+    "font-semibold text-md",
+    titleClassName
+  );
+
+  return onClick ? (
+    <button className={cardClassName} onClick={onClick}>
+      <p className={cardTitleClassName}>{title}</p>
+      <p className="font-semibold text-xs">{subtitle}</p>
+      <div className="mt-2">{children}</div>
+    </button>
+  ) : (
+    <div className={cardClassName} onClick={onClick}>
+      <p className={cardTitleClassName}>{title}</p>
       <p className="font-semibold text-xs">{subtitle}</p>
       <div className="mt-2">{children}</div>
     </div>
